52eb05e05110430befec2ee59d21259e448df1dd
Sphesihlebjamile <sphesihlebjamile@gmail.com>
2025-04-13 15:20:24 +0200

    Updated default mode

--- modes/defaultmode.py
+++ modes/defaultmode.py
@@ -1,8 +1,10 @@
 from commands.commands import Commands
+from utility.colors import Colors
 
 class DefaultMode:
     def __init__(self):
         print("Initiating default mode")
 
     def run(self):
-        print("Welcome to CSV Reader")
+        print(f"{Colors.HEADER}Welcome to CSV Reader{Colors.ENDC}")
+        print(f"{Colors.CYAN}{'-' * 50}{Colors.ENDC}")
