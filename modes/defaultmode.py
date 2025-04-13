de9fc18f1405601e82a9580932dea96e029ffc7d
Sphesihlebjamile <sphesihlebjamile@gmail.com>
2025-04-13 16:46:35 +0200

    Completed the get_user_input() funtion which reads the input from the user and performs certain functionality.

--- modes/defaultmode.py
+++ modes/defaultmode.py
@@ -1,6 +1,7 @@
 from commands.commands import Commands
 from commands.history import CommandHistory
 from utility.colors import Colors
+from utility.utility import Utility
 
 class DefaultMode:
     def __init__(self):
@@ -9,3 +10,19 @@ class DefaultMode:
     def run(self):
         print(f"{Colors.HEADER}Welcome to CSV Reader{Colors.ENDC}")
         print(f"{Colors.CYAN}{'-' * 50}{Colors.ENDC}")
+
+        while True:
+            try:
+                prompt = f'{Colors.BOLD}--csv-reader${Colors.ENDC}'
+                command = Utility.get_user_input(prompt, self.history)
+
+                if not command:
+                    continue
+
+                continue
+
+            except KeyboardInterrupt:
+                print(f'\n\n{Colors.GREEN}Thank you for using CSV Reader!{Colors.ENDC}')
+                break
+            except Exception as e:
+                print(f'{Colors.FAIL}Error: {str(e)}{Colors.ENDC}')
